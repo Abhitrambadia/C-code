c549406f64b1cdc0fde7bc63d8572580fc990f9c
Abhi trambadia <141342178+Abhitrambadia@users.noreply.github.com>
2023-08-05 10:28:25 +0530

    Add files via upload
    This was practical work 1

---
+++ p1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,sum;
+	float avg;
+	cout<<"enter a number ::";
+	cin >>a;
+	cout<<"enter a number ::";
+	cin >>b;
+	cout<<"sum="<<a+b<<endl;
+	sum=a+b;
+	cout<<"avg="<<sum/2;
+	return 0;
+}
